3927d09de39985bad01153674773033f5b6a7642
msaadm290 <muhammadsmunaf@gmail.com>
2024-01-24 22:25:18 -0600

    update all

---
+++ learning/assets/js/external.js
@@ -0,0 +1,8 @@
+function turnonBulb() {
+    document.getElementById('myImage').src = './assets/images/pic_bulbon.png'
+}
+
+function turnoffBulb() {
+    document.getElementById('myImage').src = './assets/images/pic_bulboff.jpg'
+}
+
